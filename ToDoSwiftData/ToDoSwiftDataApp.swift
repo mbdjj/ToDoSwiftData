b8d7a5ea8f55c348d91dde7885ab785384965e48
Marcin Bartmiński <mbdjj@o2.pl>
2023-07-28 08:29:15 +0200

    Initial Commit

---
+++ ToDoSwiftData/ToDoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataApp.swift
+//  ToDoSwiftData
+//
+//  Created by Marcin Bartminski on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
